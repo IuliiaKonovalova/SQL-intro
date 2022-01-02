be8501f03e606008e59970c289f734bb585e9148
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-02 15:40:02 +0800

    Add expressions

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -1,10 +1,25 @@
 import psycopg2
 
+
 # connect to chinook db
 connection = psycopg2.connect(database="chinook")
 
 # a cursor(set/list) object of the db
 cursor = connection.cursor()
 
+cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
 # Fetch the results(multiple)
 results = cursor.fetchall()
+
+# fetch sing result
+#result = cursor.fetchone()
+
+# close the connections
+connection.close()
+
+# print the results
+
+for result in results:
+    print(result)
+
